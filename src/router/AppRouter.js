3d613574e97703004e4955cc35b8943cfc4ec948
anand <anand@vizru.com>
2021-08-02 21:47:51 +0530

    initial commit

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -6,18 +6,18 @@ import BooksList from '../components/BooksList';
 import useLocalStorage from '../hooks/useLocalStorage';
 import EditBook from '../components/EditBook';
 import BooksContext from '../context/BooksContext';
-import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
-import { SearchProvider, Results, SearchBox } from "@elastic/react-search-ui";
-import { Layout } from "@elastic/react-search-ui-views";
+// import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
+// import { SearchProvider, Results, SearchBox } from "@elastic/react-search-ui";
+// import { Layout } from "@elastic/react-search-ui-views";
 
-import "@elastic/react-search-ui-views/lib/styles/styles.css";
+// import "@elastic/react-search-ui-views/lib/styles/styles.css";
 
-const connector = new AppSearchAPIConnector({
-  searchKey: "search-371auk61r2bwqtdzocdgutmg",
-  engineName: "search-ui-examples",
-  endpointBase: "http://127.0.0.1:3002",
-  cacheResponses: false
-});
+// const connector = new AppSearchAPIConnector({
+//   searchKey: "search-371auk61r2bwqtdzocdgutmg",
+//   engineName: "search-ui-examples",
+//   endpointBase: "http://127.0.0.1:3002",
+//   cacheResponses: false
+// });
 
 const AppRouter = () => {
   const [books, setBooks] = useLocalStorage('books', []);
